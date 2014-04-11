fe0bc23d73e40c7768a9cf53d510b3b9c66e2f04
Farah Juma <fjuma@redhat.com>
2014-04-11 16:31:11 -0400

    Using <f:actionListener binding> instead of <f:actionListener type>  allows the tests in ActionListenerCdiTest to pass.

--- jsf/actionlistener-injection/src/main/java/org/jboss/jsf/listeners/MyActionListener.java
+++ jsf/actionlistener-injection/src/main/java/org/jboss/jsf/listeners/MyActionListener.java
@@ -1,13 +1,18 @@
 package org.jboss.jsf.listeners;
 
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.jboss.jsf.testbeans.BackingBean;
 import org.jboss.jsf.testbeans.MyInterceptor;
 
+@Named
 public class MyActionListener implements ActionListener {
 	
 	
@@ -23,6 +28,15 @@ public class MyActionListener implements ActionListener {
 	public void processAction(ActionEvent event)
 			throws AbortProcessingException {
 		bean.addInjection();
+                try {
+                        String path = FacesContext.getCurrentInstance()
+                                        .getExternalContext().getApplicationContextPath();
+                        FacesContext.getCurrentInstance().getExternalContext()
+                                        .redirect(path.concat("/second.jsf"));
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+
 	}
 
 }
